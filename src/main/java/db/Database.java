a85524003938197a7c7f0e5208cd566d14bcbc25
carolin randala <carolinrandala@gmail.com>
2022-12-19 20:57:54 +0200

    updating items

--- src/main/java/db/Database.java
+++ src/main/java/db/Database.java
@@ -1,4 +1,4 @@
-package dp;
+package db;
 
 import java.sql.Connection;
 import java.sql.DriverManager;
--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -1,40 +1,14 @@
 package org.example;
 
-import dp.Database;
-
-import java.sql.Connection;
-import java.sql.PreparedStatement;
-import java.sql.ResultSet;
-import java.sql.SQLException;
-import java.util.Scanner;
+import controllers.Item;
 
 public class Main {
     public static void main(String[] args) {
-        getAllItems();
+        Item.getAllItems();
+        //Item.createNewItem();
+        //Item.getAllItems();
+        Item.updateItem();
+        Item.getAllItems();
     }
 
-    public static void getAllItems() {
-        Connection connection = Database.DbConn();
-        PreparedStatement ps;
-        ResultSet rs;
-        Scanner scanner = new Scanner(System.in);
-
-        try{
-            ps = connection.prepareStatement("SELECT * FROM items");
-            rs = ps.executeQuery();
-
-            //Loop through the result set
-            while(rs.next()) {
-                String id = "id: " + rs.getInt("id");
-                String name = "name: " + rs.getString("name");
-                String desc = "desc: " + rs.getString("description");
-                String qty = "qty: " + rs.getInt("qty_in_stock");
-                String price = "price: " + rs.getFloat("price");
-                System.out.println(id + " " + name + " " + desc + " " + qty + " " + price);
-            }
-        } catch (SQLException e) {
-            e.printStackTrace();
-        }
-
-    }
 }
